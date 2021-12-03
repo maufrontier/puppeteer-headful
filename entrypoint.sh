6b29544cb8395cd8b91854062b5453d48277bdfe
Mau Fournier <mau.fournier@gmail.com>
2021-12-03 00:44:32 -0600

    use bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Startup Xvfb
 Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
@@ -6,6 +6,8 @@ Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
 # Export some variables
 export DISPLAY=:99.0
 export PUPPETEER_EXEC_PATH="google-chrome-stable"
+
+bash --version
 
 # Run commands
 for task in "$@"; do {
