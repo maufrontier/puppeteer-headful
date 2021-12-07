efe3d8550b6de215fc6dc52e37b9711466f41eb9
Mau Frontier <1924890+maufrontier@users.noreply.github.com>
2021-12-07 09:39:05 -0600

    Allow background command arguments (#1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Startup Xvfb
 Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
@@ -7,14 +7,5 @@ Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
 export DISPLAY=:99.0
 export PUPPETEER_EXEC_PATH="google-chrome-stable"
 
-# Run commands
-cmd=$@
-echo "Running '$cmd'!"
-if $cmd; then
-    # no op
-    echo "Successfully ran '$cmd'"
-else
-    exit_code=$?
-    echo "Failure running '$cmd', exited with $exit_code"
-    exit $exit_code
-fi
+# Run the commands — inside a subshell for safety.
+(eval "$@")
