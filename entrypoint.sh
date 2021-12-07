74db49d745bc553dbac50ac70c503a427476f693
Mau Fournier <mau.fournier@gmail.com>
2021-12-07 09:35:20 -0600

    docs: clarify why we use eval in subshell

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
 export DISPLAY=:99.0
 export PUPPETEER_EXEC_PATH="google-chrome-stable"
 
-# Run commands
+# Run the commands — inside a subshell for safety.
 (eval "$@")
